2c2b428818dc9990aa1c9f4f7b7dcfbe56c3bb07
Pizza <pinyadapizza@gmail.com>
2022-06-08 18:11:40 +0700

    Login and home UI

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,16 +1,16 @@
 import 'package:flutter_social_ui/models/post_model.dart';
 
 class User {
-  final String profileImageUrl;
-  final String backgroundImageUrl;
-  final String name;
-  final int following;
-  final int followers;
-  final List<Post> posts;
-  final List<Post> favorites;
+  late final String profileImageUrl;
+  late final String? backgroundImageUrl;
+  late final String? name;
+  late final int? following;
+  late final int? followers;
+  late final List<Post>? posts;
+  late final List<Post>? favorites;
 
   User({
-    this.profileImageUrl,
+    this.profileImageUrl = "",
     this.backgroundImageUrl,
     this.name,
     this.following,
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/data/data.dart';
+import 'package:flutter_social_ui/widgets/following_users.dart';
+import 'package:flutter_social_ui/widgets/posts_carousel.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+
+  late TabController _tabController;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _pageController = PageController(initialPage: 0, viewportFraction: 0.8);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        title: Text(
+          'FRENZY',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 10.0,
+          ),
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorWeight: 3.0,
+          labelColor: Theme.of(context).primaryColor,
+          labelStyle: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+          ),
+          unselectedLabelStyle: TextStyle(
+            fontSize: 18.0,
+          ),
+          tabs: <Widget>[
+            Tab(text: 'Trending',),
+            Tab(text: 'Latest',),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          FollowingUsers(),
+          PostsCarousel(
+            pageController: _pageController,
+            titile: 'Posts',
+            posts: posts,
+          ),
+        ],
+      ),
+    );
+  }
+}
