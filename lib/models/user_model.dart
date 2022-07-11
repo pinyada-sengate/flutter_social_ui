9d0a6dfbe2c7589972f6e8c18a8b514d0b228b4c
Pizza <pinyadapizza@gmail.com>
2022-07-11 20:56:51 +0200

    menu bar

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,8 +2,8 @@ import 'package:flutter_social_ui/models/post_model.dart';
 
 class User {
   late final String profileImageUrl;
-  late final String? backgroundImageUrl;
-  late final String? name;
+  late final String backgroundImageUrl;
+  late final String name;
   late final int? following;
   late final int? followers;
   late final List<Post>? posts;
@@ -11,8 +11,8 @@ class User {
 
   User({
     this.profileImageUrl = "",
-    this.backgroundImageUrl,
-    this.name,
+    this.backgroundImageUrl = "",
+    this.name = "",
     this.following,
     this.followers,
     this.posts,
